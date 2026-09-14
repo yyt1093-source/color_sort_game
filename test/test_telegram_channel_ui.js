289156cbcf650ebad996dbd95397f866a10ebd95
yyt1093-source <yyt1093@gmail.com>
2026-09-14 22:19:06 +0300

    Remove subtitle 'Новости, обновления и промокоды' and bump v=48740

--- test/test_telegram_channel_ui.js
+++ test/test_telegram_channel_ui.js
@@ -16,7 +16,7 @@ assert(html.includes('src="referral_art_clean.jpg"'), 'referral_art_clean.jpg ja
 assert(html.includes('id="telegramChannelJoinBtn"'), 'telegramChannelJoinBtn must exist in index.html');
 assert(html.includes('id="telegramChannelLink"'), 'telegramChannelLink must exist in index.html');
 assert(html.includes('id="tgChannelThumb"'), 'tgChannelThumb must exist in index.html');
-assert(html.includes('app.js?v=48730'), 'app.js must be v=48730');
+assert(html.includes('app.js?v=48740'), 'app.js must be v=48740');
 console.log('  ✅ [PASS] index.html structure verified');
 
 // 2. Check style.css
